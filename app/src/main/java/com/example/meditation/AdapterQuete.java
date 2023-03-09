b28c54b3c9edd089f1ef08633f3175da960c6a22
ФасхиеваДР <dashaf712@mail.ru>
2023-03-09 17:33:54 +0300

    цитаты

--- app/src/main/java/com/example/meditation/AdapterQuete.java
+++ app/src/main/java/com/example/meditation/AdapterQuete.java
@@ -2,9 +2,20 @@ package com.example.meditation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class AdapterQuete extends AppCompatActivity {
+
+    protected Context mContext;
+    List<MaskQuete> queteList;
+
+    public AdapterQuete(Context mContext, List<MaskQuete> queteList) {
+        this.mContext = mContext;
+        this.queteList = queteList;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
